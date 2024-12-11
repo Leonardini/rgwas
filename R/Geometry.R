fb860344de709b4c00d5e21a2e008d029fd054d9
Leonid Chindelevitch <2537328+Leonardini@users.noreply.github.com>
2024-12-11 10:44:18 +0000

    Minor fixes

--- R/Geometry.R
+++ R/Geometry.R
@@ -1,10 +1,8 @@
-#' Find a short piecewise linear function that ensures that all integer points below it lie on or below the input-defined one.
+#' Find a minimal piecewise linear function that ensures that all integer points below it lie on or below the input-defined one.
 #' Return the breakpoints of this function, which are guaranteed to be as few as possible in number (see the original paper).
-#' if dryRun = TRUE, only return the extreme points to be used, pruned for collinearity; otherwise computes the best boundary.
-#' If verify = TRUE, also check that the input points lie below (above) the output points but no more than 1 below (above) them.
 #' The width specifies the width of the "tunnel" used; for best results, it should be close to 1, but strictly smaller than it.
 #' Use the standard Imai-Iri algorithm of given width.
-callImaiIri = function(extremePoints, verify = FALSE, width = WIDTH, dryRun = FALSE, CPP = TRUE, fname = "Test.csv") {
+callImaiIri = function(extremePoints, width = WIDTH, CPP = TRUE, fname = "Test.csv") {
   stopifnot(width <= 1)
   if (nrow(extremePoints) == 0) {
     return(list(breakpoints = NA, inputSize = 0, input = extremePoints))
@@ -23,41 +21,43 @@ callImaiIri = function(extremePoints, verify = FALSE, width = WIDTH, dryRun = FA
     if (nrow(points) <= 2) {
       breakpoints <- points
     } else {
-      readr::write_csv(points, fname)
-      executable_path <- system.file("bin", "plfoptq", package = "rgwas")
-      breakpoints <- system2(executable_path, args = c(fname, width/2), stdout = TRUE) %>%
-        stringr::str_split_fixed(",", 2) %>%
-        magrittr::set_colnames(c("Total", "Sum")) %>%
-        tibble::as_tibble() %>%
-        dplyr::mutate_all(as.numeric)
+      executable_path <- system.file("bin", "plfoptq", package = utils::packageName())
+      if (file.exists(executable_path)) {
+        readr::write_csv(points, fname)
+        breakpoints <- system2(executable_path, args = c(fname, width/2), stdout = TRUE) %>%
+          stringr::str_split_fixed(",", 2) %>%
+          magrittr::set_colnames(c("Total", "Sum")) %>%
+          tibble::as_tibble() %>%
+          dplyr::mutate_all(as.numeric)
+      } else {
+        print("The compiled version of the C++ code is not available; falling back on the pure R option.")
+        CPP = FALSE
+      }
     }
-  } else {
+  }
+  if (!CPP) {
     extremePoints %<>%
       removeCollinear %>%
       dplyr::mutate_at("Sum", ~{magrittr::add(., 1/2)})
   }
   pointsP <- extremePoints
-  if (!dryRun) {
-    pointsM <- extremePoints
-    pointsP %<>%
-      dplyr::mutate(Sum = Sum + width/2)
-    pointsM %<>%
-      dplyr::mutate(Sum = Sum - width/2)
-    if (!CPP) {
-      breakpoints <- ImaiIriAlgorithm(pointsP, pointsM)
-    }
-  } else {
-    breakpoints <- extremePoints
+  pointsM <- extremePoints
+  pointsP %<>%
+    dplyr::mutate(Sum = Sum + width/2)
+  pointsM %<>%
+    dplyr::mutate(Sum = Sum - width/2)
+  if (!CPP) {
+    breakpoints <- ImaiIriAlgorithm(pointsP, pointsM)
   }
-  if (verify && !dryRun && nrow(breakpoints) > 0) { # check that the required bounds are satisfied by the output
-    print("Verification of correctness")
-    pointsL <- pointsM %>%
-      dplyr::mutate(Sum = round(Sum))
-    pointsU <- pointsP %>%
-      dplyr::mutate(Sum = round(Sum))
-    stopifnot(checkPosition(breakpoints, pointsL, below = TRUE))
-    stopifnot(checkPosition(breakpoints, pointsU, below = FALSE))
-  }
+  # if (nrow(breakpoints) > 0) { # check that the required bounds are satisfied by the output
+  #   print("Verification of correctness")
+  #   pointsL <- pointsM %>%
+  #     dplyr::mutate(Sum = round(Sum))
+  #   pointsU <- pointsP %>%
+  #     dplyr::mutate(Sum = round(Sum))
+  #   stopifnot(checkPosition(breakpoints, pointsL, below = TRUE))
+  #   stopifnot(checkPosition(breakpoints, pointsU, below = FALSE))
+  # }
   if (nrow(breakpoints) > 0) {
     if (min(breakpoints$Sum) != firstPoint$Sum) {
       breakpoints %<>%
